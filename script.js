41e8133f457915e729a1f7adbc6b4382d103f527
Dmytro <dmitriy.pryshliak@gmail.com>
2024-10-31 15:21:12 +0200

    Created index page

---
+++ script.js
@@ -0,0 +1,25 @@
+const initThemeFromLocalStorage = localStorage.getItem("mode");
+
+if (!initThemeFromLocalStorage) {
+    const currentHour = new Date().getHours();
+    const defaultTheme = currentHour > 18 || currentHour < 8 ? "dark" : "light";
+    localStorage.setItem("mode", defaultTheme);
+    document.body.classList.add(defaultTheme);
+} else {
+    document.body.classList.add(initThemeFromLocalStorage)
+}
+
+const themeToggleElement = document.querySelector(".theme-toggle");
+
+const toggleTheme = () => {
+    const mode = localStorage.getItem("mode");
+    const newMode = mode === "light" ? "dark" : "light";
+    localStorage.setItem("mode", newMode);
+    if (newMode === "dark") {
+        document.body.classList.replace("light", "dark");
+    } else {
+        document.body.classList.replace("dark", "light");
+    }
+}
+
+themeToggleElement.addEventListener("click", toggleTheme)
